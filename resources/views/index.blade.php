b60b9a4b95afe885acf2578cfd5ff0a4e6c00e72
hananhanafi76 <hananhanafi76@gmail.com>
2019-12-05 04:36:32 +0700

    Config postgreSQL and create home view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts/main')
+
+@section('title','Home')
+
+@section('container')
+
+    <!-- <div class="container">
+    <div class="capt row text-center d-flex align-content-center">
+        <div class="col">
+            <h1 class="">Selamat datang di blog Hanan Hanafi</h1>
+        </div>
+    </div>
+    </div> -->
+
+    <div class="capt ">
+        <div class="center text-center">
+            <h1>Selamat Datang di Blog Hanan Hanafi</h1>
+        </div>
+        <img src="{{asset('/images/bg.png')}}" alt="" style="width: 50%">
+
+    </div>
+    
+
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <link
+            rel="stylesheet"
+            href="{{ asset('/css/bootstrap-4.4.1-dist/css/bootstrap.min.css') }}">
+
+        <link
+            rel="stylesheet"
+            href="{{ asset('/css/my.css') }}">
+        <title>@yield('title')</title>
+    </head>
+    <body>
+        <nav class="fixed-top navbar navbar-expand-lg navbar-light ">
+            <div class="container">
+                <a class="navbar-brand" href="#">Hanan Hanafi</a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home
+                                <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Categories</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        @yield('container')
+
+        <script
+            src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+            crossorigin="anonymous"></script>
+        <script
+            src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"></script>
+        <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
+    </body>
+</html>
